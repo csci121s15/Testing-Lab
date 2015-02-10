8d624620814e65ccdb8e8be88ffdf427756381e5
Josh Russett <stjkr02@moravian.edu>
2015-02-10 13:49:24 -0500

    Completed the skeleton for the Average class.

---
+++ src/Average.java
@@ -0,0 +1,19 @@
+public class Average{
+  private double average;
+  private int count;
+  
+  class Average(){
+  }
+  
+  public void addValue(double value){
+  }
+  
+  public double getAverage(){
+  return 0;
+  }
+  
+  public int getCount(){
+  return 0;
+  }
+  
+}
