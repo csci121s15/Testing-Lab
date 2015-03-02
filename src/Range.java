49e9160268e32f7c339fb88765968a463b0e6a57
Carly Thorpe <stcat04@moravian.edu>
2015-03-02 18:07:27 -0500

    Changes made to Testing Lab. Includes answers, test files and codes.

--- src/Range.java
+++ src/Range.java
@@ -1,6 +1,8 @@
 public class Range {
   private double start;
   private double stop;
+  private double start2;
+  private double stop2;
  
   public Range(double min, double max) {
     start = min;
@@ -31,6 +33,67 @@ public class Range {
   
   public double getMax() {
     return stop;
+    
+  }
+  
+  public Range intersection(Range other) {
+    start2 = other.getMin();
+    stop2 = other.getMax();
+    
+    if (start == start2 && stop == stop2) {
+      Range s = new Range(start, stop);
+      return s;
+    }
+    
+    else if (start == start2 && stop > stop2) {
+      Range s = new Range(start, stop2);
+      return s;
+    }
+    
+    else if (start == start2 && stop < stop2) {
+      Range s = new Range(start, stop);
+      return s;
+    }
+    
+    else if (stop == stop2 && start < start2) {
+      Range s = new Range(start2, stop);
+      return s;
+    }
+    
+    else if (stop == stop2 && start > start2) {
+      Range s = new Range(start, stop); 
+      return s;
+    }
+    
+    else if (start < start2 && stop < stop2) {
+      Range s = new Range(start2, stop);
+      return s;
+    }
+    
+    else if (start > start2 && stop > stop2) {
+      Range s = new Range(start, stop2);
+      return s;
+    }
+    
+    else if (start > start2 && stop < stop2) {
+      Range s = new Range(start, stop);
+      return s;
+    }
+    
+    else if (start < start2 && stop > stop2) {
+      Range s = new Range(start2, stop2);
+      return s;
+    }  
+    
+    else if (stop < start2 || stop2 < start) {
+      Range s = new Range(0,0);
+      return s;
+    }
+    
+    else {
+      Range s = new Range(0,0);
+      return s;
+    }
   }
 }
 
--- src/RangeTest.java
+++ src/RangeTest.java
@@ -5,10 +5,100 @@ public class RangeTest extends TestCase {
     Range r = new Range(0,2);
     
     assertEquals(true, r.contains(1));
-    assertEquals(2, r.getWidth());
-    assertEquals(0, r.getMin());
-    assertEquals(2, r.getMax());
+    assertEquals(2.0, r.getWidth());
+    assertEquals(0.0, r.getMin());
+    assertEquals(2.0, r.getMax());
     
   }
   
+  public void testIntersectionEqual() {
+    Range r = new Range(1.0,3.0);
+    Range t = new Range(1.0,3.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(1.0, intersect.getMin());
+    assertEquals(3.0, intersect.getMax());    
+  }
+  
+  public void testIntersectionSameStartDifferentEnd() {
+    Range r = new Range(1.0,5.0);
+    Range t = new Range(1.0,4.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(1.0, intersect.getMin());
+    assertEquals(4.0, intersect.getMax());
+  }
+  
+  public void testIntersectionSameStartDifferentStop2() {
+    Range r = new Range(1.0,4.0);
+    Range t = new Range(1.0,5.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(1.0, intersect.getMin());
+    assertEquals(4.0, intersect.getMax());
+  }
+  
+  public void testIntersectionSameStopDifferentStart() {
+    Range r = new Range(1.0,5.0);
+    Range t = new Range(2.0,5.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(2.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }
+  
+  public void testIntersectionSAmeStopDifferentStart2() {
+    Range r = new Range(2.0,5.0);
+    Range t = new Range(1.0,5.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(2.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }
+  
+  public void testIntersectionStartLessStopLess() {
+    Range r = new Range(1.0,5.0);
+    Range t = new Range(2.0,6.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(2.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }
+  
+  public void testIntersectionStartGreaterStopGreater() {
+    Range r = new Range(2.0, 6.0);
+    Range t = new Range(1.0, 5.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(2.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }
+  
+  public void testIntersectionRangeInRange2() {
+    Range r = new Range(2.0, 5.0);
+    Range t = new Range(1.0, 6.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(2.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }  
+  
+  public void testIntersectionRange2InRange1() {
+    Range r = new Range(2.0, 6.0);
+    Range t = new Range(3.0, 5.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(3.0, intersect.getMin());
+    assertEquals(5.0, intersect.getMax());
+  }
+  
+  public void testIntersectionDNE() {
+    Range r = new Range(2.0, 5.0);
+    Range t = new Range(7.0, 9.0);
+    
+    Range intersect = r.intersection(t);
+    assertEquals(0.0, intersect.getMin());
+    assertEquals(0.0, intersect.getMax());
+    
+  }
 }
